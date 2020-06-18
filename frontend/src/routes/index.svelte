b504633ef95ffdced5f5250875eb8246ddd5d243
Alexey Berezhnoy <a.berezhnoy@brandquad.ru>
2020-06-18 23:44:11 +0400

    add frontend app

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+	<title>Youtube clone</title>
+</svelte:head>
+
+{#each videos as video}
+	<div><h3>{video['name']}</h3></div>
+{/each}
+
+<script context="module">
+	const videos = [{'name': '1'}, {'name': 2}]
+</script>
