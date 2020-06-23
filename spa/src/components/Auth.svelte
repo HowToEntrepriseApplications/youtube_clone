9b9bfd6663d30b3072dd9115db90fb7f08d2a9ef
Alexey Berezhnoy <a.berezhnoy@brandquad.ru>
2020-06-23 07:41:41 +0400

    spa dummy auth

--- spa/src/components/Auth.svelte
+++ spa/src/components/Auth.svelte
@@ -11,25 +11,25 @@
 
 	export let is_authenticated = false;
 
-	let login = () => {};
-	let logout = () => {};
+	let login = () => { is_authenticated = true };
+	let logout = () => { is_authenticated = false };
 
     onMount(async () => {
-        const Keycloak = (await import('keycloak-js')).default
-        const keycloak = Keycloak({
-            url: process.env.KEYCLOAK_URL,
-            realm: process.env.KEYCLOAK_REALM,
-            clientId: process.env.KEYCLOAK_CLIENT_ID,
-        });
-
-        keycloak.init({onLoad: 'check-sso'}).success(authenticated => { is_authenticated = authenticated })
-
-        login = () => {
-            keycloak.init({onLoad: 'login-required'}).then(authenticated => { is_authenticated = authenticated })
-        }
-
-        logout = () => {
-            keycloak.logout()
-        }
+        // const Keycloak = (await import('keycloak-js')).default
+        // const keycloak = Keycloak({
+        //     url: process.env.KEYCLOAK_URL,
+        //     realm: process.env.KEYCLOAK_REALM,
+        //     clientId: process.env.KEYCLOAK_CLIENT_ID,
+        // });
+        //
+        // keycloak.init({onLoad: 'check-sso'}).success(authenticated => { is_authenticated = authenticated })
+        //
+        // login = () => {
+        //     keycloak.init({onLoad: 'login-required'}).then(authenticated => { is_authenticated = authenticated })
+        // }
+        //
+        // logout = () => {
+        //     keycloak.logout()
+        // }
 	});
 </script>
