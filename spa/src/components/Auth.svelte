cfd56eb55d8567275e2c2374b862fc380dfc61b4
Alexey Berezhnoy <a.berezhnoy@brandquad.ru>
2020-06-27 07:07:15 +0400

    add frontend auth

--- spa/src/components/Auth.svelte
+++ spa/src/components/Auth.svelte
@@ -10,26 +10,29 @@
 	import { onMount } from 'svelte';
 
 	export let is_authenticated = false;
-
-	let login = () => { is_authenticated = true };
-	let logout = () => { is_authenticated = false };
+	let login = () => {};
+	let logout = () => {};
 
     onMount(async () => {
-        // const Keycloak = (await import('keycloak-js')).default
-        // const keycloak = Keycloak({
-        //     url: process.env.KEYCLOAK_URL,
-        //     realm: process.env.KEYCLOAK_REALM,
-        //     clientId: process.env.KEYCLOAK_CLIENT_ID,
-        // });
-        //
-        // keycloak.init({onLoad: 'check-sso'}).success(authenticated => { is_authenticated = authenticated })
-        //
-        // login = () => {
-        //     keycloak.init({onLoad: 'login-required'}).then(authenticated => { is_authenticated = authenticated })
-        // }
-        //
-        // logout = () => {
-        //     keycloak.logout()
-        // }
+
+        const Keycloak = (await import('keycloak-js')).default
+        const keycloak = Keycloak({
+            url: process.env.KEYCLOAK_URL,
+            realm: process.env.KEYCLOAK_REALM,
+            clientId: process.env.KEYCLOAK_CLIENT_ID,
+        });
+
+        keycloak.init({
+            onLoad: 'check-sso',
+            silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
+        }).then(authenticated => { is_authenticated = authenticated })
+
+        login = () => {
+            keycloak.init({onLoad: 'login-required'}).then(authenticated => { is_authenticated = authenticated })
+        }
+
+        logout = () => {
+            keycloak.logout()
+        }
 	});
 </script>
